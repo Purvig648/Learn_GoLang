0bd2fc5d8f771b42ce7eccfdd640b433bdf793b4
Purvi G <purvig@BLR.ALLEGISINDIA.COM>
2025-05-19 21:08:51 +0530

    main

--- main.go
+++ main.go
@@ -1,47 +1,4 @@
 package main
 
-import (
-	"net/http"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
-)
-
-type User struct {
-	UserId   int
-	UserName string
-	Age      int
-}
-
-var user = []User{}
-
-func Start() {
-	router := gin.Default()
-	router.GET("/getUser", GetUser)
-	router.Run(":8080")
-
-}
-
-func GetUser(c *gin.Context) {
-	c.JSON(http.StatusOK, user)
-}
-
-func GetUserById(c *gin.Context) {
-	ID := c.Param("id")
-	UID, err := strconv.Atoi(ID)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-	}
-	for _, userId := range user {
-		if userId.UserId == UID {
-			c.JSON(http.StatusAccepted, UID)
-		}
-	}
-	c.JSON(http.StatusBadRequest, gin.H{
-		"error": "Not Found",
-	})
-}
-
 func main() {
-	Start()
 }
